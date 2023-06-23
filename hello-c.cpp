af6713197f1cfe69a173c1c9a8bca41505801b67
Yasin M Naal <42123587+yasinnaal@users.noreply.github.com>
2023-06-23 03:45:30 +0300

    Create hello-c.cpp

---
+++ hello-c.cpp
@@ -0,0 +1,11 @@
+// C++ program to display "Hello World"
+
+using namespace std;
+
+// Main() function: where the execution of program begins
+int main()
+{
+	cout << "Hello World C++";
+
+	return 0;
+}
